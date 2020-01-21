721f516b82168dad238f3eb7a78249da902b59eb
Micaelle Queiroz <micaellequeiroz6@gmail.com>
2020-01-21 17:25:08 -0400

    Tela de Cadastro

---
+++ CarteiradeVacinaDigital/app/src/main/java/br/fabrica/projeto/carteiravacinadigital/CadastroPrincipal.java
@@ -0,0 +1,14 @@
+package br.fabrica.projeto.carteiravacinadigital;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastroPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_principal);
+    }
+}
