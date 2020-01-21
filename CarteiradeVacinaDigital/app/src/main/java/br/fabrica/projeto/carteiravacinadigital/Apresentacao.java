94b9f122074e444f1d814be82fb8140637ea6175
Micaelle Queiroz <micaellequeiroz6@gmail.com>
2020-01-21 15:36:07 -0400

    Telas de Login e Apresentação

---
+++ CarteiradeVacinaDigital/app/src/main/java/br/fabrica/projeto/carteiravacinadigital/Apresentacao.java
@@ -0,0 +1,49 @@
+package br.fabrica.projeto.carteiravacinadigital;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+
+import br.fabrica.projeto.carteiravacinadigital.ui.login.LoginActivity;
+
+
+public class Apresentacao extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apresentacao);
+
+       //getSupportActionBar().hide();
+       getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        progressBar = findViewById(R.id.barra_progresso);
+
+        progressBar.setMax(300);
+        progressBar.setScaleY(3f);
+
+       progressoAnimacao();
+
+        //Delay para poder destruir a activity splash, pois a função que faz a chamada da activity main está em uma class java, o que impossibilita destruir a tela splash após sua execução
+         new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Após os 4 segundo inicia uma nova atividade
+                startActivity(new Intent(Apresentacao.this, LoginActivity.class));
+                finish();
+            }
+        },3000);
+    }
+
+     public void progressoAnimacao(){
+        BarraProgresso anim = new BarraProgresso(progressBar,0f,300f);
+        anim.setDuration(3000);
+        progressBar.setAnimation(anim);
+    }
+}
+
