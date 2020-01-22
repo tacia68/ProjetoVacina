98a72a616b1c39446288ae52ef8c4960e9fe76ca
Sherman <tacia68@gmail.com>
2020-01-22 00:19:57 -0400

    add busca vacinas filtradas por nome

--- CarteiradeVacinaDigital/app/src/main/java/br/fabrica/projeto/carteiravacinadigital/ListarVacinasActivity.java
+++ CarteiradeVacinaDigital/app/src/main/java/br/fabrica/projeto/carteiravacinadigital/ListarVacinasActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,12 +37,47 @@ public class ListarVacinasActivity extends AppCompatActivity {
         registerForContextMenu(listView);
 
     }
+    //Aplicar o menu principal na lista Vacinas
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater i = getMenuInflater();
+        i.inflate(R.menu.menu_principal, menu);
+
+        SearchView sv = (SearchView) menu.findItem(R.id.app_bar_search).getActionView();
+        sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+
+                return false;
+            }
+
+           //verifica as letras que o usuario digitar
+            @Override
+            public boolean onQueryTextChange(String s) {
+                procurarVacina(s);
+                return false;
+            }
+        });
+        return true;
+    }
+
+    //Buscar os valores digitados
+    public void procurarVacina(String tipoVacina){
+        vacinasFiltradas.clear();
+        for(Vacina v : vacinas){
+            if (v.getTipoVacina().toLowerCase().contains(tipoVacina.toLowerCase())){
+                vacinasFiltradas.add(v);
+            }
+
+        }
+        listView.invalidateViews();
+    }
 
     //clicar no icone "+" chama a tela de cadastro de vacina
     public void cadastrar(MenuItem item){
-        Intent it = new Intent(this, Vacina.class);
+        Intent it = new Intent(this, VacinaActivity.class);
         startActivity(it);
     }
+
 
     // atualizar a lista de vacinas cadastradas quando voltar
 
